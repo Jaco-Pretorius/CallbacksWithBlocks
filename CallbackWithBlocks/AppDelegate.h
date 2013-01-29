3b8e5497da40ff3fc29fc853aa78b42f52c96f33
Jaco Pretorius <jaco.pretorius@thoughtworks.com>
2013-01-28 19:29:55 -0500

    Initial Commit

---
+++ CallbackWithBlocks/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CallbackWithBlocks
+//
+//  Created by Jaco on 1/28/13.
+//  Copyright (c) 2013 Jaco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CallbackWithBlocks/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CallbackWithBlocks
+//
+//  Created by Jaco on 1/28/13.
+//  Copyright (c) 2013 Jaco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
